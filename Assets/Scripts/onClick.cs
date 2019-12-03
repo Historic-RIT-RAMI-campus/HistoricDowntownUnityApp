6c260076ea021d27e26367ab6d66e969b5378349
jzimmer5 <42787474+jzimmer5@users.noreply.github.com>
2019-12-03 09:10:36 -0500

    On click was added

---
+++ Assets/Scripts/onClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onClick : MonoBehaviour
+{
+    int noClick = 0;
+    public GameObject[] objList;
+    
+    public void btnClick()
+    {
+        print("in function");
+       if(objList[noClick].activeSelf)
+        {
+            objList[noClick].SetActive(false);
+            noClick++;
+
+            if (noClick >= objList.Length)
+            {
+                noClick = 0;
+            }
+            objList[noClick].SetActive(true);
+        }
+    }
+}
